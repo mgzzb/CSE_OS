5dce09adffca81b14a0123f9b32826427de2af85
mgzzb <mgonza32@nd.edu>
2023-01-23 02:28:10 +0000

    Initialize

---
+++ Milestones/Milestone01/bytecheck.c
@@ -0,0 +1,29 @@
+/**********************************
+ * Name: Andres Gonzalez and Mariana Gonzalez 
+ * Email: agonza42@nd.edu mgonza32@nd.edu 
+ * File Name: bytecheck.c
+ * Date Created: Jan 22, 2023
+ * File Contents:
+    Confirm that the file is under a given size 
+    Count how often a particular byte value occurs in a file    
+ **********************************/
+
+int main( const int argc, const char* argv[] ){
+
+	//If argc not equal to zero enter for loop
+	if(argc != 2){
+		fprintf(stdout, "Error, you did not enter two arguments.\n"); // Print error
+		exit(EXIT_FAILURE); // exit program
+	}
+
+	if(argv[1] == NULL){ //Check if file exsists
+	    fprintf(stdout,"Cannot open file: %s", argv[1]); // Print error
+	    exit(EXIT_FAILURE); // exit program
+	}
+
+	FILE* input_file = fopen(argv[1],"r"); // Create FILE* and store file
+
+	fclose( input_file );//close file
+
+	return 0;
+}
