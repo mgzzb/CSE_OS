fc9bf91ca42b74e9b9495682e4b69dbe344dcfa2
agonza42 <agonza42@nd.edu>
2023-01-26 18:45:49 -0500

    Starting Project01

---
+++ Project01/README.md
@@ -0,0 +1,8 @@
+Team Members
+
+Andres Gonzalez (agonza42@nd.edu)
+Mariana Gonzalez (mgonza32@nd.edu)
+
+|
+
+Level 2
---
+++ Project01/bitflip.c
@@ -0,0 +1,21 @@
+/**********************************
+ * Name: Andres Gonzalez and Mariana Gonzalez 
+ * Email: agonza42@nd.edu mgonza32@nd.edu 
+ * File Name: bitflip.c
+ * Date Created: Jan 26, 2023
+ * File Contents:
+    Level 2: Given a file within a certain size limit (25 kB or specified), 
+    output a bit-flipped and reverse byte-ordered file with an appended extension (.rbf 
+    or .r).
+ **********************************/
+
+// Include required libraries
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main( const int argc, const char* argv[] ){
+
+	return 0;
+
+}
