1a36cdba782803ae90195b85bf71002c8e923ad5
agonza42 <agonza42@nd.edu>
2023-02-12 23:08:06 -0500

    Start Project02

---
+++ Project02/ndshell.c
@@ -0,0 +1,11 @@
+/**********************************
+ * Name: Andres Gonzalez and Mariana Gonzalez 
+ * Email: agonza42@nd.edu mgonza32@nd.edu 
+ * File Name: ndshell.c
+ * Date Created: Feb 12, 2023
+ * File Contents:
+    You will write a program called ndshell which is capable of executing, managing, and 
+    monitoring user level programs. This program will be similar in purpose and design to everyday 
+    shells like bash or tcsh, although the syntax will be slightly different.  ndshell will be 
+    invoked without any arguments, and will support several different commands that include: 
+ **********************************/
